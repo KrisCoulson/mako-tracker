568e136594e8f5f97ac70b15028a8deaeb12ab66
Kris Coulson <kriscoulson@gmail.com>
2021-10-28 00:19:10 -0700

    switch migrations to postgresql

---
+++ api/db/migrations/20211028071845_add_event/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" SERIAL NOT NULL,
+    "type" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    PRIMARY KEY ("id")
+);
