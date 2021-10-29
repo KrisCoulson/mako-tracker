0d26e5a2cca053282df27d5f8274a024f33bc427
Kris Coulson <kriscoulson@gmail.com>
2021-10-28 19:22:09 -0700

    update query filter

--- api/src/services/events/events.js
+++ api/src/services/events/events.js
@@ -1,24 +1,14 @@
 import { db } from 'src/lib/db'
-import { startOfDay, endOfDay } from 'date-fns'
+import { startOfToday, startOfTomorrow } from 'date-fns'
 import { zonedTimeToUtc} from 'date-fns-tz'
 export const events = () => {
-  // const date = zonedTimeToUtc(new Date('2021-10-28'), 'America/Los_Angeles')
-  // console.log({
-  //   date,
-  //   test: new Date('2021-10-28'),
-  //   gte: startOfDay(date),
-  //   lt:  endOfDay(date),
-  //   oldGte: startOfDay(new Date('2021-10-28')),
-  //   oldLt: endOfDay(new Date('2021-10-28'))
-  // })
 
-  const date = new Date('2021-10-29')
   return db.event.findMany(
     {
       where: {
         createdAt: {
-          gte: startOfDay(date),
-          lt:  endOfDay(date)
+          gte: startOfToday(),
+          lt:  startOfTomorrow()
         },
       },
       orderBy: {
